d5df357ed0df5ea885dcd58c1aa1ac2957fd7339
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-21 20:54:18 +0200

    step 3

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Amir Bakhshi on 2021-08-21.
+//  Copyright © 2021 Baam.dev. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func recalcPressed(_ sender: UIButton) {
+    }
+
+
+}
